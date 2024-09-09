0b2298a97ba2b227e77b1d5fc99202c80c193d5f
= <nicolasaguirrebruno01@gmail.com>
2024-09-09 09:29:07 -0300

    feat: fixed navigation

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -1,12 +1,10 @@
-//TODO: IMPLEMENTAR
+import { Navigate } from 'react-router-dom';
+import { AuthContext } from '../contexts';
+import { useContext } from 'react';
 
-// import { Navigate, Routes } from 'react-router-dom';
-// import { AuthContext } from '../contexts';
-// import { useContext } from 'react';
-
-// export const PublicRoutes = ({ children }) => {
-//   const { auth } = useContext(AuthContext);
-//   const { logged } = auth;
-//   console.log(logged)
-//   return !logged ? children : <Navigate to="inicio" />;
-// };
+export const PublicRoutes = ({ children }) => {
+  const { auth } = useContext(AuthContext);
+  const { logged } = auth;
+  console.log(logged)
+  return !logged ? children : <Navigate to="inicio" />;
+};
