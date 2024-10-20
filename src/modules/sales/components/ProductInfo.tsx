5c191db83da26346f41ac218e2eed8cf5c00d0e8
unknown <micaelarrigonifranco@gmail.com>
2024-10-20 15:56:37 -0300

    feat:sales-summary-add

--- src/modules/sales/components/ProductInfo.tsx
+++ src/modules/sales/components/ProductInfo.tsx
@@ -1,6 +1,7 @@
 import { Icon } from '@iconify/react/dist/iconify.js';
 import styles from '../styles/sales.module.scss';
 import { mopsusIcons } from '../../../icons';
+import { useState } from 'react';
 
 export const ProductInfo = ({ product }) => {
     const { productName, price } = product;
